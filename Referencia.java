58252525edba5a94df69ca68370f6e2cfa388a0b
Jose Antonio Perez Cano <100277627+JoselitoJose@users.noreply.github.com>
2022-04-21 09:32:44 +0200

    Add files via upload

---
+++ Referencia.java
@@ -0,0 +1,21 @@
+package JOSE;
+//JOSE ANTONIO PEREZ CANO
+
+public class Referencia {
+    public int x;
+
+    public Referencia() {
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    /*public String toString() {
+        return this.x.makeConcatWithConstants<invokedynamic>(this.x);
+    }*/
+}
